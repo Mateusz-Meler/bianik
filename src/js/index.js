a6b41ad433df2d986f71ce28816b224e8b846fe4
Mateusz Meler <mateuszmeler@icloud.com>
2020-12-27 12:22:58 +0100

    fixing hamburger menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,24 @@
 import "../scss/main.scss";
 import SmoothScroll from "smooth-scroll";
 var scroll = new SmoothScroll('a[href*="#"]');
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav__box");
+  const navLinks = document.querySelectorAll(".nav__box li");
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`;
+      }
+    });
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
