34bc112cc1c8b6f63b2f43c90c3334a0defa6d79
Mateusz Meler <mateuszmeler@icloud.com>
2021-01-04 23:45:15 +0100

    fixing all bugs

--- src/js/index.js
+++ src/js/index.js
@@ -21,4 +21,20 @@ const navSlide = () => {
   });
 };
 
+const counters = document.querySelectorAll(".div__header");
+const speed = 400;
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+    const inc = target / speed;
+    if (count < target) {
+      counter.innerText = Math.ceil(count + inc);
+      setTimeout(updateCount, 1);
+    } else {
+      count.innerText = target;
+    }
+  };
+  updateCount();
+});
 navSlide();
